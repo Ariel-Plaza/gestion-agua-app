2e19f2da1e79a2e531b40040662a4f1fae7448ae
arielplazasalinas <arielplazasalinas@gmail.com>
2026-03-17 12:14:43 -0300

    feat: agregar modelos Socio, Ruta y Medidor con registro en admin

---
+++ gestionaguaApp/socios/models/medidor.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .socio import Socio
+
+class Medidor(models.Model):
+    socio_id= models.ForeignKey(Socio, on_delete=models.CASCADE)
+    numero_medidor = models.CharField(max_length=10 ,null=False, unique=True )
+    estado_servicio = models.CharField(max_length=10 ,null=False)
+    fecha_instalacion = models.DateField(null=True, blank=True)
---
+++ gestionaguaApp/socios/models/socio.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .ruta import Ruta
+
+class Socio(models.Model):
+    numero_socio = models.IntegerField(unique=True)
+    # agregar validar de rut segun el -k
+    rut = models.CharField(max_length=10, unique=True)
+    nombre = models.CharField(max_length=30, null=False)
+    apellido = models.CharField(max_length=30,null=False)
+    telefono = models.CharField(max_length=15, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    ruta_id = models.ForeignKey(Ruta, on_delete=models.CASCADE)
+    referencia_direccion = models.CharField(max_length=500)
+    subsidio = models.BooleanField(null=False, default=False)
+    activo = models.BooleanField(null=False, default=True)
+    fecha_registro = models.DateField(null=False, auto_now_add=True)
