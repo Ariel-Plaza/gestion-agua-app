f84cab5f259e5797fdbaf77b1b9c90673aa58b18
arielplazasalinas <arielplazasalinas@gmail.com>
2026-03-24 19:10:32 -0300

    feat:corregir logica de busqueda por segundo apellido

--- gestionaguaApp/socios/views.py
+++ gestionaguaApp/socios/views.py
@@ -6,7 +6,7 @@ from .models import Socio
 from .serializer import SocioSerializer
 
 # Create your views here.
-
+        
 class ListaSocios(APIView):
     def get(self, request):
         socios = Socio.objects.all()
@@ -18,25 +18,27 @@ class ObtenerSocioNombreApellidos(APIView):
             
         nombre = request.query_params.get('nombre')
         apellido = request.query_params.get('apellido')
-        segundo_apellido = request.query_params.get('segundo_apellido', None)  # opcional
+        s_apellido = request.query_params.get('s_apellido', None) # opcional
+        
         if(nombre is None or apellido is None):
                 return Response(
                     {'error': 'debes ingresar un nombre y un apellido'},
                                 status=status.HTTP_404_NOT_FOUND)
-        # Filtra por nombre y apellido
-        socio = Socio.objects.filter(nombre=nombre)
-        if socio.count() > 1:
-            socio = socio.filter(apellido=apellido)
+        else:
+            # Filtra por nombre y apellido
+            socio = Socio.objects.filter(nombre=nombre,apellido=apellido)
+            # Si encuentra mas de un nombre y apellido igual
             if socio.count() > 1:
-                if segundo_apellido is None:
+                if s_apellido is None:
+                    # solicita segundo apellido
                     return Response({'error': 'existen múltiples socios, agrega el segundo apellido'})
                 else:
-                    socio = socio.filter(segundo_apellido=segundo_apellido)
-
-        if socio.exists():
-            serializer = SocioSerializer(socio, many=True)
-            return Response(serializer.data)
-        else:
-            return Response(
-                {'error': 'no existe un socio con el nombre buscado'},
-                status=status.HTTP_404_NOT_FOUND)
+                    socio = socio.filter(segundo_apellido = s_apellido)
+            # verifica que socio existe
+            if socio.exists():
+                serializer = SocioSerializer(socio, many=True)
+                return Response(serializer.data)
+            else:
+                return Response(
+                    {'error': 'no existe un socio con el nombre buscado'},
+                    status=status.HTTP_404_NOT_FOUND)
