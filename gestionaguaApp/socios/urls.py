ed67c71e580a882cf3530a3245b4e2a933416983
arielplazasalinas <arielplazasalinas@gmail.com>
2026-03-27 17:11:10 -0300

    feat: agregar CRUD completo modulo socios

--- gestionaguaApp/socios/urls.py
+++ gestionaguaApp/socios/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import ListaSocios, ObtenerSocioNombreApellidos
+from .views import AgregarSocio, ListaSocios, ObtenerSocioNombreApellidos, ActualizarSocio, EliminarSocio
 
 urlpatterns = [
     path('', ListaSocios.as_view(), name='lista-socios'),
     path('buscar/', ObtenerSocioNombreApellidos.as_view(), name='buscar-socio'),
+    path('agregar/', AgregarSocio.as_view(), name='agregar-socio'),
+    path('actualizar/<int:pk>', ActualizarSocio.as_view(), name='actualizar-socio'),
+    path('eliminar/<int:pk>', EliminarSocio.as_view(), name='eliminar-socio'),
+    
 ]
--- gestionaguaApp/socios/views.py
+++ gestionaguaApp/socios/views.py
@@ -6,10 +6,21 @@ from .models import Socio
 from .serializer import SocioSerializer
 
 # Create your views here.
-        
+class AgregarSocio(APIView):
+    def post(self, request):
+        # deserializar JSON
+        serializer = SocioSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, 
+                            status=status.HTTP_400_BAD_REQUEST)
+            # return Response(exception=True,status=status.HTTP_404_NOT_FOUND,data=True)
+
 class ListaSocios(APIView):
     def get(self, request):
-        socios = Socio.objects.all()
+        socios = Socio.objects.exclude(activo=False)
         serializer = SocioSerializer(socios, many=True)
         return Response(serializer.data)
     
@@ -26,19 +37,47 @@ class ObtenerSocioNombreApellidos(APIView):
                                 status=status.HTTP_404_NOT_FOUND)
         else:
             # Filtra por nombre y apellido
-            socio = Socio.objects.filter(nombre=nombre,apellido=apellido)
+            socio = Socio.objects.filter(nombre=nombre,apellido=apellido, activo=True)
             # Si encuentra mas de un nombre y apellido igual
             if socio.count() > 1:
                 if s_apellido is None:
                     # solicita segundo apellido
                     return Response({'error': 'existen múltiples socios, agrega el segundo apellido'})
                 else:
-                    socio = socio.filter(segundo_apellido = s_apellido)
-            # verifica que socio existe
-            if socio.exists():
-                serializer = SocioSerializer(socio, many=True)
-                return Response(serializer.data)
-            else:
+                    socio = socio.filter(segundo_apellido = s_apellido, activo= True)
+            # Si soy no existe
+            if not socio.exists():
                 return Response(
                     {'error': 'no existe un socio con el nombre buscado'},
                     status=status.HTTP_404_NOT_FOUND)
+            
+            serializer = SocioSerializer(socio, many=True)
+            return Response(serializer.data)
+                
+class ActualizarSocio(APIView):
+    def put(self,request,pk):
+        try:
+            actualizar =Socio.objects.get(pk=pk)
+            serializer = SocioSerializer(actualizar, data = request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    {'error': 'los datos no son validos'},
+                    status=status.HTTP_400_BAD_REQUEST)
+        except Socio.DoesNotExist:
+            return Response({'error':'Socio no encontrado'}, 
+                            status=status.HTTP_404_NOT_FOUND)
+            
+
+class EliminarSocio(APIView):
+    def delete(self,request,pk):
+        try:
+            socio =Socio.objects.get(pk=pk)
+        except Socio.DoesNotExist:
+            return Response({'error':'Socio no encontrado'}, 
+                            status=status.HTTP_404_NOT_FOUND)
+        socio.activo = False
+        socio.save()
+        return Response({'status':'Socio desactivado correctamente'},status=status.HTTP_200_OK)
