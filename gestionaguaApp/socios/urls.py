505b7fe5d81e058e6a8b1d5be285f3789bdd7fca
arielplazasalinas <arielplazasalinas@gmail.com>
2026-03-27 17:18:44 -0300

    feat: se corrige urls con parametros

--- gestionaguaApp/socios/urls.py
+++ gestionaguaApp/socios/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', ListaSocios.as_view(), name='lista-socios'),
     path('buscar/', ObtenerSocioNombreApellidos.as_view(), name='buscar-socio'),
     path('agregar/', AgregarSocio.as_view(), name='agregar-socio'),
-    path('actualizar/<int:pk>', ActualizarSocio.as_view(), name='actualizar-socio'),
-    path('eliminar/<int:pk>', EliminarSocio.as_view(), name='eliminar-socio'),
+    path('actualizar/<int:pk>/', ActualizarSocio.as_view(), name='actualizar-socio'),
+    path('eliminar/<int:pk>/', EliminarSocio.as_view(), name='eliminar-socio'),
     
 ]
