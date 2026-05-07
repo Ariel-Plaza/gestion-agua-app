9086a7282ded864973f13bc73842065cad7e0045
arielplazasalinas <arielplazasalinas@gmail.com>
2026-05-07 10:11:42 -0300

    feat: agrega modelo Usuario extendido de AbstractUSer

--- gestionaguaApp/usuarios/models.py
+++ gestionaguaApp/usuarios/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 # username, password, email, first_name, last_name
+# extiendo de AbstractUser
+class Usuario(AbstractUser):
+    # defino roles
+    ROL_CHOICES = [
+        ('administrador', 'Administrador'),
+        ('operador','Operador'),
+        ('funcionario','Funcionario'),
+        ('socio','Socio')
+    ]
+    #defino campo rol y asigno ROL_CHOICES
+    rol = models.CharField(
+        max_length=20,
+        choices=ROL_CHOICES,
+        default='socio'
+    )
